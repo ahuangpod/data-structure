b52918321fde4e983defcbd7c94d9ab7e8a14a25
Aiden Huang <ahuang@Aidens-Mac-mini.local>
2021-06-13 11:41:45 +0800

    Binary search

--- binary_search.py
+++ binary_search.py
@@ -3,10 +3,12 @@ def binary_search(lst, target):
     right = len(lst) - 1
     while (left < right):
         mid = (left + right) // 2
-        if target == mid:
+        if target == lst[mid]:
             return mid
         elif target < mid:
             right = mid
         else:
             left = mid
     return None
+
+print(binary_search([1,2,3,4,5], 3))
